6f14e27b1302eb95f733fd8f71ac5dbabf0b15ff
Thananon Pongsuwan <thenightraven7407@gmail.com>
2021-03-17 13:07:52 +0200

    Add put logic to staff.js and check if it's already exist

--- routes/staff.js
+++ routes/staff.js
@@ -11,6 +11,7 @@ router.get('/getAll', function (req, res, next) {
     })
 });
 
+//Registration
 router.post('/add', async function (req, res) {
     const collection = db.get().collection('staff')
     let exist = await collection.find({email: req.body.email}).toArray()
@@ -24,6 +25,7 @@ router.post('/add', async function (req, res) {
 
 })
 
+//Delete account
 router.delete('/delete/:email', async function (req, res) {
     let email = req.params.email
     const collection = db.get().collection('staff')
@@ -34,6 +36,25 @@ router.delete('/delete/:email', async function (req, res) {
         collection.deleteOne({email: email})
         res.send("Data Deleted")
     }
+})
+
+//Edit account
+router.put('/edit/:email', async function (req, res) {
+    let email = req.params.email
+    const collection = db.get().collection('staff')
+    let oldStaff = await collection.find({email: email}).toArray()
+    if (oldStaff[0] === undefined) {
+        res.send("There is no data to be edited")
+    } else {
+        try {
+            await collection.updateOne({email: email}, {$set: req.body})
+            res.send("Edit completed")
+        } catch (e) {
+            console.log(e)
+            res.send(e)
+        }
+    }
+
 })
 
 module.exports = router;
