f14f2dc9ab16c780223f9057a2fe5680c5eac4c4
Thananon Pongsuwan <thenightraven7407@gmail.com>
2021-03-29 15:47:45 +0300

    update authorRoute.js

---
+++ routes/authorRoute.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const {getAllAuthor, editAuthor, addNewAuthor, deleteAuthor} = require('../controller/bookController')
+
+
+/* GET users listing. */
+router.get('/getAll', getAllAuthor)
+//Registration
+router.post('/add', addNewAuthor)
+//Delete account
+router.delete('/delete/:id', deleteAuthor)
+//Edit account
+router.put('/edit/:id', editAuthor)
+
+
+module.exports = router;
